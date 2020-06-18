7681d5ee474e1f6e35bcb1575ca700403d34f7a6
ruthbins <ruthbins@yahoo.com>
2020-06-18 17:00:03 +0300

    Added normalize

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function addTransaction(e) {
 
 // Generate random ID
 function generateID() {
-  return Math.floor(Math.random() * 100000000);
+  return Math.floor(Math.random() * 10000000);
 }
 
 // Add transactions to DOM list
